82576a716ce80237533af953af1e88530fedc1c4
Chris Bartlett <chris@wecreate.com.hk>
2020-01-14 14:18:24 +0800

    updated html lang tag

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -29,6 +29,7 @@ const SEO = ({ title, description, image, keywords}) => (
                     <meta name="image" content={seo.image} />
                     <meta name="keywords" content={seo.keywords} />
                     <meta name="robots" content="index,follow" />
+                    <html lang="en" />
                 </Helmet>
               )
         }}
